1bb47ed8552316f9e1001dcb42521324060047cf
HanselVinicius <hanelvinicius@gmail.com>
2023-12-13 15:31:22 -0300

    enviroment variables fix

--- src/app/infra/clients/ExceptionsApiClient.ts
+++ src/app/infra/clients/ExceptionsApiClient.ts
@@ -1,12 +1,13 @@
 import {Injectable } from "@angular/core";
 import axios, { AxiosInstance } from "axios";
+import { environment } from "src/environments/environment";
 
 
 @Injectable({
 	providedIn: "root"
 })
 export class ExceptionApiClient{
-    private readonly API = '';
+    private readonly API = environment.API_EXCEPTIONS;
     private axiosClient:AxiosInstance
 
     constructor(){
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  API_URL: process.env['API_URL_JOGO_DO_BIXO']
+  API_URL: process.env['API_URL_JOGO_DO_BIXO'],
+  API_EXCEPTIONS:process.env['API_EXCEPTIONS']
 };
